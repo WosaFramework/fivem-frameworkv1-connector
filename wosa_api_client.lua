7663c96970e92a8907229ed043141e1c423f4a28
Valdemar Hägglund <39058565+Clatanii@users.noreply.github.com>
2020-04-24 12:49:06 +0200

    Update wosa_api_client.lua

--- wosa_api_client.lua
+++ wosa_api_client.lua
@@ -9,8 +9,24 @@
 -- @ARRAY API REGISTER
 Wosa = nil
 
+-- @LOADING VAR
+local LoadingConnectorVersion = nil
+
 ------------------------------
 --- API (FROM WOSA) EVENTS ---
 ------------------------------
 
-TriggerEvent('WOSA:USER_DATA:LIBRARY:SERVER:GET_DATA', function(data) Wosa = data end)
+CreateThread(function()
+    TriggerServerEvent('WOSA:PUBLIC_API:GET_TYPE_TO_CLIENT')
+    
+    while LoadingConnectorVersion == nil do
+        Wait(0)
+    end
+    
+    TriggerEvent('WOSA:USER_DATA:LIBRARY:SERVER:GET_DATA'..LoadingConnectorVersion, function(data) Wosa = data end)
+end)
+
+RegisterNetEvent('WOSA:PUBLIC_API:GET_TYPE_TO_CLIENT')
+AddEventHandler('WOSA:PUBLIC_API:GET_TYPE_TO_CLIENT', function()
+	TriggerClientEvent('WOSA:PUBLIC_API:GET_TYPE_TO_CLIENT', source, WOSA_API.CONFIG.API_CONNECTOR_TYPE)
+end)
