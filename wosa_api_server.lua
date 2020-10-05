fe2f5217ca6049288a9e68acbe93c896b37d89a9
Clatanii <39058565+Clatanii@users.noreply.github.com>
2020-10-05 16:45:04 +0200

    Update wosa_api_server.lua

--- wosa_api_server.lua
+++ wosa_api_server.lua
@@ -4,31 +4,6 @@
 
 
 
-
-
---[[
-
-	"v2" is strongly adviced...
-	"v1" is still supported but not maintained by any core developer(s)...
-
---]]
---- --- --- --- --- --- --- --- ---
------------------------------------
---- --- --- --- --- --- --- --- ---
-
-local version = 'v2'
-
---- --- --- --- --- --- --- --- ---
------------------------------------
---- --- --- --- --- --- --- --- ---
-
-
-
-
-
-
-
-
 --------------------------
 --- API CONFIG PRIVATE ---
 --------------------------
@@ -49,7 +24,7 @@ WOSA_API.CONFIG = {
 --- API (FROM WOSA) EVENTS ---
 ------------------------------
 
-TriggerEvent('WOSA:USER_DATA:LIBRARY:SERVER:GET_DATA_'..version or 'v1', function(data) Wosa = data end)
+TriggerEvent('WOSA:USER_DATA:LIBRARY:SERVER:GET_DATA_v2', function(data) Wosa = data end)
 
 --------------------------------
 --- API (MAIN EVENTS) EVENTS ---
@@ -70,3 +45,49 @@ AddEventHandler('onResourceStop', function(resource)
 		TriggerEvent('WOSA:API_SYNC:DATA_WRITE', source, 'rem_resource', send_data)
 	end
 end)
+
+-------------------
+--- TRANSLATION ---
+-------------------
+
+t = function(key, data1, data2, data3, data4, data5, data6)
+	if data1 == nil then
+		return Wosa.Translate.Get()[WOSA_API.CONFIG.API_SYNC_RESOURCE][Wosa.Translate.Language()][key] or Wosa.Translate.Get()[WOSA_API.CONFIG.API_SYNC_RESOURCE]['en'][key] or key
+	else
+		if Wosa.Translate.Get()[WOSA_API.CONFIG.API_SYNC_RESOURCE][Wosa.Translate.Language()][key] == nil and Wosa.Translate.Get()[WOSA_API.CONFIG.API_SYNC_RESOURCE]['en'][key] == nil then
+			return key
+		else
+			local cache = Wosa.Translate.Get()[WOSA_API.CONFIG.API_SYNC_RESOURCE][Wosa.Translate.Language()][key] or Wosa.Translate.Get()[WOSA_API.CONFIG.API_SYNC_RESOURCE]['en'][key]
+
+			if data1 ~= nil then
+				cache = cache:gsub('#data1', data1)
+			end
+
+			if data2 ~= nil then
+				cache = cache:gsub('#data2', data2)
+			end
+			
+			if data3 ~= nil then
+				cache = cache:gsub('#data3', data3)
+			end
+			
+			if data4 ~= nil then
+				cache = cache:gsub('#data4', data4)
+			end
+			
+			if data5 ~= nil then
+				cache = cache:gsub('#data5', data5)
+			end
+			
+			if data5 ~= nil then
+				cache = cache:gsub('#data5', data5)
+			end
+
+			if data6 ~= nil then
+				cache = cache:gsub('#data6', data6)
+			end
+
+			return cache
+		end
+	end
+end
